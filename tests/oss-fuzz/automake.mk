ec694af4516e04a507d9d1d91c45db35d428ee32
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-07-10 10:57:58 +0200

    Add oss-fuzz

---
+++ tests/oss-fuzz/automake.mk
@@ -0,0 +1,21 @@
+OSS_FUZZ_TARGETS = \
+	tests/oss-fuzz/flow_extract_target \
+	tests/oss-fuzz/json_parser_target \
+	tests/oss-fuzz/ofp_print_target
+EXTRA_PROGRAMS += $(OSS_FUZZ_TARGETS)
+oss-fuzz-targets: $(OSS_FUZZ_TARGETS)
+
+tests_oss_fuzz_flow_extract_target_SOURCES = tests/oss-fuzz/flow_extract_target.c
+tests_oss_fuzz_flow_extract_target_LDADD = lib/libopenvswitch.la
+
+tests_oss_fuzz_json_parser_target_SOURCES = tests/oss-fuzz/json_parser_target.c
+tests_oss_fuzz_json_parser_target_LDADD = lib/libopenvswitch.la
+
+tests_oss_fuzz_ofp_print_target_SOURCES = tests/oss-fuzz/ofp_print_target.c
+tests_oss_fuzz_ofp_print_target_LDADD = lib/libopenvswitch.la
+
+EXTRA_DIST += \
+	tests/oss-fuzz/config/flow_extract_fuzzer.options \
+	tests/oss-fuzz/config/json_parser_fuzzer.options \
+	tests/oss-fuzz/config/ofp_print_fuzzer.options \
+	tests/oss-fuzz/config/ovs.dict
