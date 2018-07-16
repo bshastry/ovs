6cab280fc86e6bef856d2cd023901a5d7f4ed9a3
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-07-16 10:34:24 +0200

    oss-fuzz: LDADD LIB_FUZZING_ENGINE for linking fuzz tests

--- tests/oss-fuzz/automake.mk
+++ tests/oss-fuzz/automake.mk
@@ -8,20 +8,21 @@ oss-fuzz-targets: $(OSS_FUZZ_TARGETS)
 tests_oss_fuzz_flow_extract_target_SOURCES = \
 	tests/oss-fuzz/flow_extract_target.c \
 	tests/oss-fuzz/fuzzer.h
-tests_oss_fuzz_flow_extract_target_LDADD = lib/libopenvswitch.la
-tests_oss_fuzz_flow_extract_target_LDFLAGS = "-lFuzzingEngine"
+tests_oss_fuzz_flow_extract_target_LDADD = lib/libopenvswitch.la \
+					   $(LIB_FUZZING_ENGINE)
 
 tests_oss_fuzz_json_parser_target_SOURCES = \
 	tests/oss-fuzz/json_parser_target.c \
 	tests/oss-fuzz/fuzzer.h
-tests_oss_fuzz_json_parser_target_LDADD = lib/libopenvswitch.la
-tests_oss_fuzz_json_parser_target_LDFLAGS = "-lFuzzingEngine"
+tests_oss_fuzz_json_parser_target_LDADD = lib/libopenvswitch.la \
+					  $(LIB_FUZZING_ENGINE)
 
 tests_oss_fuzz_ofp_print_target_SOURCES = \
 	tests/oss-fuzz/ofp_print_target.c \
 	tests/oss-fuzz/fuzzer.h
-tests_oss_fuzz_ofp_print_target_LDADD = lib/libopenvswitch.la
-tests_oss_fuzz_ofp_print_target_LDFLAGS = "-lFuzzingEngine"
+tests_oss_fuzz_ofp_print_target_LDADD = lib/libopenvswitch.la \
+					$(LIB_FUZZING_ENGINE)
+
 
 EXTRA_DIST += \
 	tests/oss-fuzz/config/flow_extract_target.options \
