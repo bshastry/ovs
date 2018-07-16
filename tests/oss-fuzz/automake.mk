a4773932bc640b3da3550dac2f5e1eaf97fb0360
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-07-16 13:39:16 +0200

    oss-fuzz: Try 2

--- tests/oss-fuzz/automake.mk
+++ tests/oss-fuzz/automake.mk
@@ -16,11 +16,15 @@ tests_oss_fuzz_json_parser_target_SOURCES = \
 	tests/oss-fuzz/json_parser_target.c \
 	tests/oss-fuzz/fuzzer.h
 tests_oss_fuzz_json_parser_target_LDADD = lib/libopenvswitch.la
+tests_oss_fuzz_json_parser_target_LDFLAGS = $(LIB_FUZZING_ENGINE) \
+					     -lc++
 
 tests_oss_fuzz_ofp_print_target_SOURCES = \
 	tests/oss-fuzz/ofp_print_target.c \
 	tests/oss-fuzz/fuzzer.h
 tests_oss_fuzz_ofp_print_target_LDADD = lib/libopenvswitch.la
+tests_oss_fuzz_ofp_print_target_LDFLAGS = $(LIB_FUZZING_ENGINE) \
+					     -lc++
 
 EXTRA_DIST += \
 	tests/oss-fuzz/config/flow_extract_target.options \
