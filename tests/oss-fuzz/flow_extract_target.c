684a5e662e7f51322f2ac6730a99d858ab2ff5ef
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-08-23 17:20:21 +0200

    oss-fuzz: extended flow extract test case with more APIs from lib/flow.c

--- tests/oss-fuzz/flow_extract_target.c
+++ tests/oss-fuzz/flow_extract_target.c
@@ -9,11 +9,25 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     struct dp_packet packet;
     struct flow flow;
+    struct flowmap fmap;
     struct match match;
+    struct match flow_metadata;
     struct ofp10_match ext_match;
 
     dp_packet_use_const(&packet, data, size);
     flow_extract(&packet, &flow);
+
+    // Extract flowmap
+    flow_wc_map(&flow, &fmap);
+
+    // Extract metadata
+    flow_get_metadata(&flow, &flow_metadata);
+
+    // Hashing functions
+    uint32_t hash = flow_hash_5tuple(&flow, 0);
+    hash = flow_hash_symmetric_l4(&flow, 0);
+    hash = flow_hash_symmetric_l2(&flow, 0);
+    hash = flow_hash_symmetric_l3l4(&flow, 0, NULL);
 
     // Convert flow to match
     match_wc_init(&match, &flow);
