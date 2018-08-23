847ea2cebe163f57603e68fd5cdfb8ec1f433a96
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-08-23 16:29:29 +0200

    oss-fuzz: Added flow to match conversion to flow extract test

--- tests/oss-fuzz/flow_extract_target.c
+++ tests/oss-fuzz/flow_extract_target.c
@@ -8,8 +8,24 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     struct dp_packet packet;
     struct flow flow;
+    struct match match;
+    struct ofp10_match ext_match;
 
     dp_packet_use_const(&packet, data, size);
     flow_extract(&packet, &flow);
+
+    // Convert flow to match
+    match_wc_init(&match, &flow);
+    ofputil_match_to_ofp10_match(&match, &ext_match);
+
+    // Print match and packet
+    char *ext_s = ofp10_match_to_string(&ext_match, NULL, 2);
+    ofp_print_packet(stdout, dp_packet_data(&packet), dp_packet_size(&packet), htonl(PT_ETH));
+    ovs_hex_dump(stdout, dp_packet_data(&packet), dp_packet_size(&packet), 0, true);
+    match_print(&match, NULL);
+    printf("Actually extracted flow:\n%s\n", ext_s);
+    ovs_hex_dump(stdout, &ext_match, sizeof ext_match, 0, false);
+    free(ext_s);
+
     return 0;
 }
