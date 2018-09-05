4f158db222094a97268b8c61886db4bc8bd483bc
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-09-05 15:05:03 +0200

    ossfuzz: Extended flow extract test harness by including calls to more flow related APIs

--- tests/oss-fuzz/flow_extract_target.c
+++ tests/oss-fuzz/flow_extract_target.c
@@ -2,14 +2,52 @@
 #include "fuzzer.h"
 #include "dp-packet.h"
 #include "flow.h"
+#include "openvswitch/match.h"
 
 int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     struct dp_packet packet;
     struct flow flow;
+    struct flowmap fmap;
+    struct match match;
+    struct match flow_metadata;
+    struct ofp10_match ext_match;
+    char *ext_s = NULL;
+
+    if (size == 0) {
+        return 0;
+    }
 
     dp_packet_use_const(&packet, data, size);
     flow_extract(&packet, &flow);
+
+    // Extract flowmap
+    flow_wc_map(&flow, &fmap);
+
+    // Extract metadata
+    flow_get_metadata(&flow, &flow_metadata);
+
+    // Hashing functions
+    uint32_t hash = flow_hash_5tuple(&flow, 0);
+    hash = flow_hash_symmetric_l4(&flow, 0);
+    hash = flow_hash_symmetric_l2(&flow, 0);
+    hash = flow_hash_symmetric_l3l4(&flow, 0, NULL);
+
+    // Convert flow to match
+    match_wc_init(&match, &flow);
+    ofputil_match_to_ofp10_match(&match, &ext_match);
+
+    // Print match and packet
+    ext_s = ofp10_match_to_string(&ext_match, NULL, 2);
+    ofp_print_packet(stdout, dp_packet_data(&packet), dp_packet_size(&packet), htonl(PT_ETH));
+    ovs_hex_dump(stdout, dp_packet_data(&packet), dp_packet_size(&packet), 0, true);
+    match_print(&match, NULL);
+    if (ext_s) {
+        printf("Actually extracted flow:\n%s\n", ext_s);
+        free(ext_s);
+    }
+
+    ovs_hex_dump(stdout, &ext_match, sizeof ext_match, 0, false);
     return 0;
 }
