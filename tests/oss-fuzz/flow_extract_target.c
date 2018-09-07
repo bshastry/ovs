020cdbb3ae92266959cc13d855709b6870fc76fa
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-09-07 15:55:57 +0200

    ossfuzz: Add miniflow APIs to flow extract

--- tests/oss-fuzz/flow_extract_target.c
+++ tests/oss-fuzz/flow_extract_target.c
@@ -6,6 +6,45 @@
 #include "openvswitch/ofp-print.h"
 #include "openvswitch/match.h"
 
+static void test_miniflow(struct flow *flow)
+{
+    struct miniflow *miniflow;
+    struct miniflow *miniflow, *miniflow2, *miniflow3;
+    struct flow flow2, flow3;
+    int i;
+
+    const uint64_t *flow_u64 = (const uint64_t *) flow;
+
+    /* Convert flow to miniflow. */
+    miniflow = miniflow_create(flow);
+
+    /* Check that the flow equals its miniflow. */
+    for (i = 0; i < FLOW_MAX_VLAN_HEADERS; i++) {
+        assert(miniflow_get_vid(miniflow, i) ==
+               vlan_tci_to_vid(flow.vlans[i].tci));
+    }
+    for (i = 0; i < FLOW_U64S; i++) {
+        assert(miniflow_get(miniflow, i) == flow_u64[i]);
+    }
+
+    /* Check that the miniflow equals itself. */
+    assert(miniflow_equal(miniflow, miniflow));
+
+    /* Convert miniflow back to flow and verify that it's the same. */
+    miniflow_expand(miniflow, &flow2)
+    assert(flow_equal(&flow, &flow2));
+    /* Check that copying a miniflow works properly. */
+    miniflow2 = miniflow_clone__(miniflow);
+    assert(miniflow_equal(miniflow, miniflow2));
+    assert(miniflow_hash__(miniflow, 0) == miniflow_hash__(miniflow2, 0));
+    miniflow_expand(miniflow2, &flow3);
+    assert(flow_equal(&flow, &flow3));
+
+    free(miniflow);
+    free(miniflow2);
+    free(miniflow3);
+}
+
 int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
@@ -17,6 +56,15 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     /* Extract flowmap. */
     struct flowmap fmap;
     flow_wc_map(&flow, &fmap);
+
+    /* Test miniflow. */
+    test_miniflow(&flow);
+
+    /* Parse TCP flags. */
+    if (dp_packet_size(&packet) >= ETH_HEADER_LEN) {
+        uint16_t tcp_flags = parse_tcp_flags(&packet);
+        ignore(tcp_flags);
+    }
 
     /* Extract metadata. */
     struct match flow_metadata;
